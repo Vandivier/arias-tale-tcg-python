b063ff91e305d0943a82001544ad9973c986417b
Vandivier <vandivi3r@gmail.com>
2024-05-26 16:43:43 -0400

    feat: set global pyodide

--- src/main.py
+++ src/main.py
@@ -1,5 +1,32 @@
-def greet(name):
-    return f"Hello, {name}!"
+import pygame
+import sys
 
 
-print(greet("Pyodide"))
+def main():
+    pygame.init()
+
+    canvas = pyodide.globals.get("canvas")
+    width, height = canvas.width, canvas.height
+
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pygame in Pyodide")
+
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 255))
+        pygame.draw.rect(screen, (255, 255, 255), (100, 100, 200, 200))
+
+        pygame.display.flip()
+        clock.tick(30)
+
+    pygame.quit()
+    sys.exit()
+
+
+main()
