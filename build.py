ca022862e4ad8947a395ad6c9b3e956b71905685
Vandivier <vandivi3r@gmail.com>
2024-05-26 16:03:56 -0400

    feat: hello pyodide

--- build.py
+++ build.py
@@ -1,18 +1,28 @@
+import os
+import shutil
+
+
 def main():
+    # Replace the placeholder with the Python script content
     with open("src/main.py", "r") as script_file:
         python_script = script_file.read()
 
-    with open("index_template.html", "r") as template_file:
+    with open("src/index_template.html", "r") as template_file:
         html_template = template_file.read()
 
     html_content = html_template.replace(
         "<!-- PYTHON_SCRIPT_PLACEHOLDER -->", python_script
     )
 
-    with open("index.html", "w") as output_file:
+    # Write output to dist
+    os.makedirs("dist", exist_ok=True)
+    with open("dist/index.html", "w") as output_file:
         output_file.write(html_content)
 
-    print("index.html has been created with the embedded Python script.")
+    shutil.copy("src/index.css", "dist/index.css")
+
+    # Print success message
+    print("dist/index.html and dist/index.css have been created.")
 
 
 if __name__ == "__main__":
