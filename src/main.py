b939d34bf8bc6d972ed97faa0fea318f2d22593c
Vandivier <vandivi3r@gmail.com>
2024-06-14 22:50:26 -0400

    feat: alter back color on keydown

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import pygame
+import random
 
 
 async def main():
@@ -12,13 +13,20 @@ async def main():
     pygame.display.set_caption("Pygame in Pyodide")
 
     running = True
+    fill_color = (0, 0, 255)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                fill_color = (
+                    random.randint(0, 255),
+                    random.randint(0, 255),
+                    random.randint(0, 255),
+                )
 
-        screen.fill((0, 0, 255))
+        screen.fill(fill_color)
         pygame.draw.rect(screen, (255, 255, 255), (100, 100, 200, 200))
         pygame.display.flip()
         await asyncio.sleep(1 / 30)  # 30 FPS
